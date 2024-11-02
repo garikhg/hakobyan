447ef18ac51652cc4398cd060f4c7d04e10b1aec
garikhg <garik.28h@gmail.com>
2024-11-02 21:12:44 +0400

    Added Component divider

---
+++ frontend/src/components/ui/divider.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Divider = () => {
+    return <div className="flex w-full py-4 my-4 before:block before:h-px before:w-1/2 before:bg-slate-100 after:block after:h-px after:w-1/2 after:bg-slate-100"></div>;
+};
+
+export default Divider;
